426b300bdd3cc69d8a2ea959b1f57a3cb054dce7
Zhao-Le <40663829+Zhao-Le@users.noreply.github.com>
2018-09-17 21:10:08 +0800

    Add files via upload

---
+++ name.py
@@ -0,0 +1,21 @@
+name = "adab lovelace"
+print(name.title())    
+name = "Ada Lovelace"
+print(name.upper())	   
+print(name.lower())    
+print("---------------")
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("---------------")
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print("Hello, " + full_name.title()+"!")
+
+print("\tPython")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
