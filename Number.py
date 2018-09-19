84373f642cc5477877a0f506b27f3fe535d6e6df
Zhao-Le <40663829+Zhao-Le@users.noreply.github.com>
2018-09-19 17:07:25 +0800

    Add files via upload

---
+++ Number.py
@@ -0,0 +1,18 @@
+#encoding=utf-8
+
+#整型数加减乘除
+print(2+3)
+print(3-2)
+#整型数乘方
+print(3**2)
+print(4**4)
+# 浮点数运算
+print(0.1+0.2)
+
+# str将()中数据类型转换成字符串类型
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+#Python之禅
+import this
